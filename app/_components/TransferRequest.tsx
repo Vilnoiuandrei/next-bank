f2a7d6bdce85592645f26e1e99facddc0df38c8d
Andrei Vilnoiu <Vilnoiuandrei@gmail.com>
2025-03-25 21:31:27 +0200

    Refactor Transfer component to use React Query for data mutation and improve error handling

--- app/_components/TransferRequest.tsx
+++ app/_components/TransferRequest.tsx
@@ -1,45 +1,60 @@
 "use client";
 
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+
+async function fetchTransfer({
+  amount,
+  recipient,
+}: {
+  amount: string;
+  recipient: string;
+}) {
+  const res = await fetch("/api/transfer", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      amount: parseFloat(amount),
+      recipient,
+    }),
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to process the transfer");
+  }
+
+  return res.json();
+}
 
 export default function Transfer() {
   const [amount, setAmount] = useState("");
   const [recipient, setRecipient] = useState("");
-  const [error, setError] = useState<string>("");
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const transfer = useMutation({
+    mutationFn: fetchTransfer,
+    onSuccess: () => {
+      setAmount("");
+      setRecipient("");
+      alert("Transfer successful!");
+    },
+    onError: (error: any) => {
+      alert(error.message || "An error occurred during the transfer.");
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (recipient.length < 5) {
-      alert("Please introduce a recipient !");
-    } else {
-      try {
-        const res = await fetch("/api/transfer", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            amount: parseFloat(amount),
-            recipient,
-          }),
-        });
-
-        if (res.ok) {
-          alert("Transfer successful");
-          //Reset form
-          setAmount("");
-          setRecipient("");
-          setError("");
-        }
-      } catch (err: any) {
-        setError(err);
-        alert(error);
-      }
+    if (!amount || !recipient) {
+      alert("Please fill in all fields.");
+      return;
     }
+    transfer.mutate({ amount, recipient });
   };
 
   return (
-    <div className="md:w-80 border-4 border-customLight rounded-lg lg:w-96 w-72  mt-12">
+    <div className="md:w-80 border-4 border-customLight rounded-lg lg:w-96 w-72 mt-12">
       <h3 className="text-3xl text-center mb-1">Transfer</h3>
       <form
         onSubmit={handleSubmit}
@@ -51,7 +66,7 @@ export default function Transfer() {
           placeholder="Amount"
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
-          className="border p-2 rounded-sm "
+          className="border p-2 rounded-sm"
         />
         <input
           type="email"
